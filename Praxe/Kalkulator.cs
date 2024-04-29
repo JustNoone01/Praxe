dceb8a38df0378c751c5ac35cfa92c4d18d03900
lukasf1 <lukasfel@proton.me>
2024-04-29 13:41:48 +0200

    add class Kalkulator

---
+++ Praxe/Kalkulator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praxe
+{
+    internal class Kalkulator
+    {
+        private int Soucet(int n)
+        {
+            int result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+
+        public void UI()
+        {
+            int n = getInput();
+            int result = Soucet(n);
+            Console.WriteLine($"Vysledek: {result}");
+        }
+
+        private int getInput()
+        {
+            int n;
+
+            while (true)
+            {
+                Console.Write("Zadej n: ");
+                bool ok = Int32.TryParse(Console.ReadLine(), out n);
+                Console.Write("\n");
+
+                if (ok)
+                {
+                    break;
+                }
+                Console.WriteLine("Chyba ve vstupu!");
+            }
+
+            return n;
+        }
+    }
+}
--- Praxe/Program.cs
+++ Praxe/Program.cs
@@ -5,46 +5,10 @@
         static void Main(string[] args)
         {
 
-            UI();
+            Kalkulator kalkulator = new Kalkulator();
+            kalkulator.UI();
 
             Console.ReadLine();
-        }
-
-        static int Soucet(int n) 
-        {
-            int result = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                result += i;
-            }
-            return result;
-        }
-
-        static void UI()
-        {
-            int n = getInput();
-            int result = Soucet(n);
-            Console.WriteLine($"Vysledek: {result}");
-        }
-
-        static int getInput()
-        {
-            int n;
-
-            while (true)
-            {
-                Console.Write("Zadej n: ");
-                bool ok = Int32.TryParse(Console.ReadLine(), out n);
-                Console.Write("\n");
-
-                if (ok)
-                {
-                    break;
-                }
-                Console.WriteLine("Chyba ve vstupu!");
-            }
-
-            return n;
         }
     }
 }
