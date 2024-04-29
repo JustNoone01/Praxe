be59b739dc19e7db4510bf81e64c4487074f5d6d
MathiasPorubsky <maty.hokejka@email.cz>
2024-04-29 12:47:19 +0200

    metody
    přidal jsem metody

--- Praxe/Program.cs
+++ Praxe/Program.cs
@@ -6,20 +6,25 @@ namespace Praxe
     {
         static void Main(string[] args)
         {
-            Soucet();
+            Uzivatel();
         }
-        static int Soucet()
+        static int Soucet(int cisloN)
         {
-            Console.WriteLine("Zadej poslední sčítané číslo: ");
-            int cisloN =Convert.ToInt32(Console.ReadLine());
-
             int result = 0;
             for (int i = 0; i <= cisloN; i++)
             {
                  result +=i;
             }
-            Console.WriteLine($"vaše číslo je: {result}");
             return result;  
         }
+        static void Uzivatel()
+        {
+            Console.WriteLine("Zadej poslední sčítané číslo: ");
+            int cisloN = Convert.ToInt32(Console.ReadLine());
+            int number = Soucet(cisloN);
+            Console.WriteLine($"vaše číslo je: {number}");
+        }
+
+        
     }
 }
