ced47154de7550ba992923f49de81df4e90aa89c
JustNoone01 <sabatajakub3@gmail.com>
2024-04-29 12:36:44 +0200

    soucet 1 to 100

--- Praxe/Program.cs
+++ Praxe/Program.cs
@@ -14,6 +14,22 @@ namespace Praxe
             Console.ReadKey();
             //comment2
             //comment3
+
+            Console.WriteLine("Napiš konečné číslo: ");
+            int konec = int.Parse(Console.ReadLine());
+            Kalkulator(konec);
+            Console.ReadKey(true);
+        }
+        static void Kalkulator(int end)
+        {
+            int start = 1;
+
+            int counter = 0;
+            for (int i = start; i <= end; i++)
+            {
+                counter = counter + i;
+            }
+            Console.WriteLine(counter);
         }
     }
 }
