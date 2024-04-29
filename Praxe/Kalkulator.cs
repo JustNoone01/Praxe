9c9a013b16719f8ddc5b2d2f27845d19728f407b
MathiasPorubsky <maty.hokejka@email.cz>
2024-04-29 13:42:31 +0200

    třída kalkulator
    přidání třídy

---
+++ Praxe/Kalkulator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praxe
+{
+    internal class Kalkulator
+    {
+        private int Soucet(int cisloN)
+        {
+            int result = 0;
+            for (int i = 0; i <= cisloN; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+        public void Uzivatel()
+        {
+            Console.WriteLine("Zadej poslední sčítané číslo: ");
+            //int cisloN;
+            bool ok = Int32.TryParse(Console.ReadLine(), out int cisloN);
+            if (!ok)
+            {
+                Console.WriteLine("Neplatný vstup");
+                Uzivatel();
+            }
+            else
+            {
+                int number = Soucet(cisloN);
+                Console.WriteLine($"vaše číslo je: {number}");
+
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
--- Praxe/Program.cs
+++ Praxe/Program.cs
@@ -6,36 +6,8 @@ namespace Praxe
     {
         static void Main(string[] args)
         {
-            Uzivatel();
-        }
-        static int Soucet(int cisloN)
-        {
-            int result = 0;
-            for (int i = 0; i <= cisloN; i++)
-            {
-                 result +=i;
-            }
-            return result;  
-        }
-        static void Uzivatel()
-        {
-            Console.WriteLine("Zadej poslední sčítané číslo: ");
-            int cisloN;
-            bool ok = Int32.TryParse(Console.ReadLine(), out cisloN);
-            if (!ok)
-            {
-                Console.WriteLine("Neplatný vstup");
-                Uzivatel();
-            }
-            else
-            {
-                int number = Soucet(cisloN);
-                Console.WriteLine($"vaše číslo je: {number}");
-
-                Console.ReadKey(true);
-            }
-        }
-
-        
+            Kalkulator kalkulator = new Kalkulator();
+            kalkulator.Uzivatel();
+        }  
     }
 }
