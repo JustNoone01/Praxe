8eee26f6a8cb7da110c1882bde7402e7f82c474b
JustNoone01 <sabatajakub3@gmail.com>
2024-04-29 15:29:11 +0200

    Fibonacci

--- Praxe/Kalkulacka.cs
+++ Praxe/Kalkulacka.cs
@@ -8,6 +8,19 @@ namespace Praxe
 {
     internal class Kalkulacka
     {
+        private void Fibonachi(int end)
+        {
+            int counter = 0;
+            int counter2 = 1;
+            int counter3;
+            for (int cykly = 0; cykly <= end; cykly++)
+            {
+                Console.WriteLine(counter + counter2);
+                counter3 = counter;
+                counter = counter2;
+                counter2 += counter3;
+            }
+        }
         private int Vypocet(int end)
         {
             int counter = 0;
@@ -26,7 +39,7 @@ namespace Praxe
                 Console.Write("Napiš konečné číslo: ");
                 success = int.TryParse(Console.ReadLine(), out end);
             }
-
+            Fibonachi(end);
             int soucet = Vypocet(end);
             Console.WriteLine($"Součet od 1 do {end}: {soucet}");
             Console.ReadKey(true);
