b4083cb438da02f47a8681c8015373c91b90d02c
JustNoone01 <sabatajakub3@gmail.com>
2024-04-29 11:27:06 +0200

    Project C#

---
+++ Praxe/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Praxe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //cau
+        }
+    }
+}
