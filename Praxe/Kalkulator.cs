d9833e8b895d051e8a04f152fe83ea959609a944
MathiasPorubsky <maty.hokejka@email.cz>
2024-04-29 15:41:44 +0200

    Fibonnachiho posloupnost
    fibonachiho posloupnost

--- Praxe/Kalkulator.cs
+++ Praxe/Kalkulator.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Praxe
 {
@@ -31,9 +33,25 @@ namespace Praxe
             {
                 int number = Soucet(cisloN);
                 Console.WriteLine($"vaše číslo je: {number}");
+                Fibonacci(cisloN);
+               
 
                 Console.ReadKey(true);
             }
         }
+        public int Fibonacci(int cisloN)
+        {
+            int a = 0, b = 1;
+            Console.Write($"{a} ");
+            Console.Write($"{b} ");
+            for (int i = 0;i < cisloN;i++)
+            {
+                int next = a + b;
+                Console.Write($"{next} ");
+                a = b;
+                b = next;
+            }
+            return cisloN;
+        }      
     }
 }
