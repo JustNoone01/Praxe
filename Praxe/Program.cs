49117494e673483f8c7d1979a2891796c74b1da0
JustNoone01 <sabatajakub3@gmail.com>
2024-04-30 12:23:22 +0200

    Hra

--- Praxe/Program.cs
+++ Praxe/Program.cs
@@ -6,14 +6,138 @@ namespace Praxe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            //cau
-            Console.WriteLine("Karel");
-            //comment
-            Console.Write("je");
-            Console.ReadKey();
-            //comment2
-            //comment3
+            string[,] Pole = new string[5, 5];
+            bool Game = true;
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    Pole[x, y] = "O";
+                }
+            }
+            int som = 0;
+
+            while (Game)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    for (int y = 0; y < 5; y++)
+                    {
+                        Console.Write(Pole[x, y] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+
+
+                Console.Write("Zadej řádek bodu který chceš změnit: ");
+                int radek;
+                while (!int.TryParse(Console.ReadLine(), out radek) || radek < 1 || radek > 5)
+                {
+                    Console.WriteLine("Zadaná volba je příliš velká nebo malá.");
+                    Console.Write($"Zadej výběr (): ");
+                }
+
+                int sloupec;
+                Console.Write("Zadej sloupec bodu který chceš změnit: ");
+                while (!int.TryParse(Console.ReadLine(), out sloupec) || sloupec < 1 || sloupec > 5)
+                {
+                    Console.WriteLine("Zadaná volba je příliš velká nebo malá.");
+                    Console.Write($"Zadej výběr (): ");
+                }
+
+                radek -= 1;
+                sloupec -= 1;
+                
+                if (Pole[radek, sloupec] == "X")
+                {
+                    Pole[radek, sloupec] = "O";
+                }
+                else
+                {
+                    Pole[radek, sloupec] = "X";
+                }
+
+                if (radek +1 > 4)
+                {
+
+                } // Dole
+                else
+                {
+                    if (Pole[radek + 1, sloupec] == "X")
+                    {
+                        Pole[radek + 1, sloupec] = "O";
+                    }
+                    else
+                    {
+                        Pole[radek + 1, sloupec] = "X";
+                    }
+                }
+
+                if (radek - 1 < 0) 
+                {
+                    
+                } // Nahoře
+                else
+                {
+                    if (Pole[radek - 1, sloupec] == "X")
+                    {
+                        Pole[radek - 1, sloupec] = "O";
+                    }
+                    else
+                    {
+                        Pole[radek - 1, sloupec] = "X";
+                    }
+
+                }
+
+                if (sloupec + 1 > 4)
+                {
+
+                } // Vpravo
+                else
+                {
+                    if (Pole[radek, sloupec + 1] == "X")
+                    {
+                        Pole[radek, sloupec + 1] = "O";
+
+                    }
+                    else
+                    {
+                        Pole[radek, sloupec + 1] = "X";
+                    }
+                }
+
+                if (sloupec - 1 < 0)
+                {
+                } // Vlevo
+                else
+                {
+                    if (Pole[radek, sloupec - 1] == "X")
+                    {
+                        Pole[radek, sloupec - 1] = "O";
+                    }
+                    else
+                    {
+                        Pole[radek, sloupec - 1] = "X";
+                    }
+                }
+
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (Pole[i, j] == "X")
+                        {
+                            Game = false;
+                        }
+                        else { break; }
+                    }
+                }
+                Console.Clear();
+            }
         }
     }
 }
