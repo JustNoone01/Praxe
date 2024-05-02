56bcd7b4379577e82f5fbb60a6d656a4fdc47c37
JustNoone01 <sabatajakub3@gmail.com>
2024-05-02 16:12:51 +0200

    box

---
+++ APIScriptEditor/APIScriptEditor/Form1.cs
@@ -0,0 +1,30 @@
+﻿using FastColoredTextBoxNS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APIScriptEditor
+{
+    public partial class Form1 : Form
+    {
+        TextStyle brownStyle = new TextStyle(Brushes.Brown, null, FontStyle.Regular);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fctb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //clear previous highlighting
+            e.ChangedRange.ClearStyle(brownStyle);
+            //highlight tags
+            e.ChangedRange.SetStyle(brownStyle, "<[^>]+>");
+        }
+    }
+}
