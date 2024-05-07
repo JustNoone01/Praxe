0c4ce7f0dbd54db2da8cf13cd70493a53546de2c
chorec <chorec@seznam.cz>
2024-05-07 11:07:23 +0200

    lepsi kod hry

--- Praxe/Program.cs
+++ Praxe/Program.cs
@@ -1,177 +1,57 @@
-﻿using System.Diagnostics;
-using System.Diagnostics.Contracts;
-using System.Reflection.Metadata;
-
-namespace Praxe
+﻿namespace Praxe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // zadani rozmeru
             Console.Write("Zadej počet řádků: ");
-            int Pocetradek;
-            while (!int.TryParse(Console.ReadLine(), out Pocetradek))
+            int pocetradek;
+            while (!(int.TryParse(Console.ReadLine(), out pocetradek) && pocetradek >= 2))
             {
                 Console.WriteLine("Zadaná volba neodpovádí formátu.");
                 Console.Write("Zadej počet řádků: ");
             }
 
-            int Pocetsloupcu;
+            int pocetsloupcu;
             Console.Write("Zadej počet sloupců: ");
-            while (!int.TryParse(Console.ReadLine(), out Pocetsloupcu))
+            while (!(int.TryParse(Console.ReadLine(), out pocetsloupcu) && pocetsloupcu >= 2))
             {
-                Console.WriteLine("Zadaná volba neodpovádí formátu.");
+                Console.WriteLine("Zadaná volba neodpovídá formátu.");
                 Console.Write("Zadej počet sloupců: ");
             }
 
-            string[,] Pole = new string[Pocetradek, Pocetsloupcu];
-            bool Game = true;
-
+            // vygenerovani nahodneho pole
+            bool[,] pole = new bool[pocetradek, pocetsloupcu];
             Random random = new Random();
-
-            for (int x = 0; x < Pocetradek; x++)
+            for (int x = 0; x < pocetradek; x++)
             {
-                for (int y = 0; y < Pocetsloupcu; y++)
+                for (int y = 0; y < pocetsloupcu; y++)
                 {
-                    Pole[x, y] = "O";
-                }
-            }
-            
-            for (int i  = 0; i < Pole.Length; i++)
-            {
-                for (int j = 0; j < Pole.Length; j++)
-                {
-                    int radnx = random.Next(0, Pocetradek);
-                    int randy = random.Next(0, Pocetsloupcu);
-
-                    if (Pole[radnx, randy] == "X")
-                    {
-                        Pole[radnx, randy] = "O";
-                    }
-                    else
-                    {
-                        Pole[radnx, randy] = "X";
-                    }
-
-                    if (radnx + 1 > Pocetradek - 1)
-                    {
-
-                    } // Dole
-                    else
-                    {
-                        if (Pole[    + 1, randy] == "X")
-                        {
-                            Pole[radnx + 1, randy] = "O";
-                        }
-                        else
-                        {
-                            Pole[radnx + 1, randy] = "X";
-                        }
-                    }
-
-                    if (radnx - 1 < 0)
-                    {
-
-                    } // Nahoře
-                    else
-                    {
-                        if (Pole[radnx - 1, randy] == "X")
-                        {
-                            Pole[radnx - 1, randy] = "O";
-                        }
-                        else
-                        {
-                            Pole[radnx - 1, randy] = "X";
-                        }
-
-                    }
-
-                    if (randy + 1 > Pocetsloupcu - 1)
-                    {
-
-                    } // Vpravo
-                    else
-                    {
-                        if (Pole[radnx, randy + 1] == "X")
-                        {
-                            Pole[radnx, randy + 1] = "O";
-
-                        }
-                        else
-                        {
-                            Pole[radnx, randy + 1] = "X";
-                        }
-                    }
-
-                    if (randy - 1 < 0)
-                    {
-                    } // Vlevo
-                    else
-                    {
-                        if (Pole[radnx, randy - 1] == "X")
-                        {
-                            Pole[radnx, randy - 1] = "O";
-                        }
-                        else
-                        {
-                            Pole[radnx, randy - 1] = "X";
-                        }
-                    }
+                    pole[x, y] = random.Next(2) == 1; // nahodne cislo 0 nebo 1 == 1
                 }
             }
 
-            while (Game)
+            // hra
+            while (true)
             {
-                for (int x = 0; x < Pocetradek; x++)
+                for (int x = 0; x < pocetradek; x++)
                 {
-                    for (int y = 0; y < Pocetsloupcu; y++)
+                    for (int y = 0; y < pocetsloupcu; y++)
                     {
-                        Console.Write(Pole[x, y] + " ");
+                        Console.Write((pole[x, y] ? "X" : "O") + " ");
                     }
                     Console.WriteLine();
                 }
 
-                int idk = 1;
-                for (int i = 0; i < Pocetradek; i++)
+                if (CheckWin(pole))
                 {
-                    for (int j = 0; j < Pocetsloupcu; j++)
-                    {
-                        if (Pole[i, j] == "X")
-                        {
-                            idk = 0;
-                        }
-                        else
-                        {
-                            idk = 1;
-                            goto fail;
-                        }
-                    }
+                    break;
                 }
-                for (int i = 0; i < Pocetradek; i++)
-                {
-                    for (int j = 0; j < Pocetsloupcu; j++)
-                    {
-                        if (Pole[i, j] == "O")
-                        {
-                            idk = 0;
-                        }
-                        else
-                        {
-                            idk = 1;
-                            goto fail;
-                        }
-                    }
-                }
-                fail:
-                if (idk == 0)
-                {
-                    Game = false;
-                }
-
 
                 Console.Write("Zadej řádek bodu který chceš změnit: ");
                 int radek;
-                while (!int.TryParse(Console.ReadLine(), out radek) || radek < 1 || radek > Pocetradek)
+                while (!int.TryParse(Console.ReadLine(), out radek) || radek < 1 || radek > pocetradek)
                 {
                     Console.WriteLine("Zadaná volba je příliš velká nebo malá.");
                     Console.Write("Zadej řádek bodu který chceš změnit: ");
@@ -179,91 +59,63 @@ namespace Praxe
 
                 int sloupec;
                 Console.Write("Zadej sloupec bodu který chceš změnit: ");
-                while (!int.TryParse(Console.ReadLine(), out sloupec) || sloupec < 1 || sloupec > Pocetsloupcu)
+                while (!int.TryParse(Console.ReadLine(), out sloupec) || sloupec < 1 || sloupec > pocetsloupcu)
                 {
                     Console.WriteLine("Zadaná volba je příliš velká nebo malá.");
                     Console.Write($"Zadej výběr (): ");
                 }
 
+                // Meni pocatek souradnic z 1 na 0
                 radek -= 1;
                 sloupec -= 1;
                 
-                if (Pole[radek, sloupec] == "X")
+                // Prebarvi policko a okolni pole
+                pole[radek, sloupec] = !pole[radek, sloupec];
+                if (radek + 1 < pocetradek)
                 {
-                    Pole[radek, sloupec] = "O";
+                    pole[radek + 1, sloupec] = !pole[radek + 1, sloupec];
                 }
-                else
+                if (radek - 1 >= 0)
                 {
-                    Pole[radek, sloupec] = "X";
+                    pole[radek - 1, sloupec] = !pole[radek - 1, sloupec];
                 }
-
-                if (radek +1 > Pocetradek-1)
+                if (sloupec + 1 < pocetsloupcu)
                 {
-
-                } // Dole
-                else
-                {
-                    if (Pole[radek + 1, sloupec] == "X")
-                    {
-                        Pole[radek + 1, sloupec] = "O";
-                    }
-                    else
-                    {
-                        Pole[radek + 1, sloupec] = "X";
-                    }
+                    pole[radek, sloupec + 1] = !pole[radek, sloupec + 1];
                 }
-
-                if (radek - 1 < 0) 
+                if (sloupec - 1 >= 0)
                 {
-                    
-                } // Nahoře
-                else
-                {
-                    if (Pole[radek - 1, sloupec] == "X")
-                    {
-                        Pole[radek - 1, sloupec] = "O";
-                    }
-                    else
-                    {
-                        Pole[radek - 1, sloupec] = "X";
-                    }
-
-                }
-
-                if (sloupec + 1 > Pocetsloupcu-1)
-                {
-
-                } // Vpravo
-                else
-                {
-                    if (Pole[radek, sloupec + 1] == "X")
-                    {
-                        Pole[radek, sloupec + 1] = "O";
-
-                    }
-                    else
-                    {
-                        Pole[radek, sloupec + 1] = "X";
-                    }
-                }
-
-                if (sloupec - 1 < 0)
-                {
-                } // Vlevo
-                else
-                {
-                    if (Pole[radek, sloupec - 1] == "X")
-                    {
-                        Pole[radek, sloupec - 1] = "O";
-                    }
-                    else
-                    {
-                        Pole[radek, sloupec - 1] = "X";
-                    }
+                    pole[radek, sloupec - 1] = !pole[radek, sloupec - 1];
                 }
 
                 Console.Clear();
             }
+
+            Console.WriteLine("Hra skončila!");
+            Console.ReadLine();
         }
+
+        // Zjisti, zda uzivatel vyhral - zda jsou vsechny symboly stejne
+        private static bool CheckWin(bool[,] pole)
+        {
+            if (pole == null)                
+                return true;
+
+            int pocetRadek = pole.GetLength(0);
+            int pocetSloupcu = pole.GetLength(1);
+            bool firstVal = pole[0, 0];
+            for (int i = 0; i < pocetRadek; i++)
+            {
+                for (int j = 0; j < pocetSloupcu; j++)
+                {
+                    if (pole[i, j] != firstVal)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
     }
 }
